043bae1618cff3b3a1a65f4dd24168a04f6efcac
Thullyoo <thullyofilho@gmail.com>
2024-11-02 23:05:57 -0300

    fix: getters e setters

--- src/main/java/br/thullyoo/lista_tarefa_back/entity/Tarefa.java
+++ src/main/java/br/thullyoo/lista_tarefa_back/entity/Tarefa.java
@@ -12,8 +12,6 @@ import java.util.Date;
 @Table(name = "TB_TAREFAS")
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 public class Tarefa {
 
     @Id
@@ -29,4 +27,43 @@ public class Tarefa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer ordem_apresentacao;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(Double custo) {
+        this.custo = custo;
+    }
+
+    public Date getData_limite() {
+        return data_limite;
+    }
+
+    public void setData_limite(Date data_limite) {
+        this.data_limite = data_limite;
+    }
+
+    public Integer getOrdem_apresentacao() {
+        return ordem_apresentacao;
+    }
+
+    public void setOrdem_apresentacao(Integer ordem_apresentacao) {
+        this.ordem_apresentacao = ordem_apresentacao;
+    }
 }
