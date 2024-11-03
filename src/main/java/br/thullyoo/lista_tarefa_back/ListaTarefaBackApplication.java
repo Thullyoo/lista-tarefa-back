629e4dee0d4891aaa425f76f5301219d5a467d80
Thullyoo <thullyofilho@gmail.com>
2024-11-02 22:52:56 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/lista_tarefa_back/ListaTarefaBackApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.lista_tarefa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaTarefaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaTarefaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/lista_tarefa_back/ListaTarefaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.lista_tarefa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaTarefaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
