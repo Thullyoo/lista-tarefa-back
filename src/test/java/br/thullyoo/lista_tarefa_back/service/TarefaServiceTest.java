bf4520fc4785162b64372645eeb70ff7d83b7985
Thullyoo <thullyofilho@gmail.com>
2025-01-18 12:48:19 -0300

    feat: add method to verify tarefa return with successfully

--- src/test/java/br/thullyoo/lista_tarefa_back/service/TarefaServiceTest.java
+++ src/test/java/br/thullyoo/lista_tarefa_back/service/TarefaServiceTest.java
@@ -1,12 +1,14 @@
 package br.thullyoo.lista_tarefa_back.service;
 
 import br.thullyoo.lista_tarefa_back.entity.DTO.TarefaDTO;
+import br.thullyoo.lista_tarefa_back.entity.Tarefa;
 import br.thullyoo.lista_tarefa_back.exceptions.DataLimiteException;
 import br.thullyoo.lista_tarefa_back.repository.TarefaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.util.Assert;
 
@@ -28,6 +30,18 @@ class TarefaServiceTest {
     public void deveLancarDataLimiteException(){
         TarefaDTO tarefaDTO = new TarefaDTO("Criar um site", "2000", LocalDate.MIN);
         assertThrows(DataLimiteException.class, () -> tarefaService.incluirTarefa(tarefaDTO));
+    }
+
+    @Test
+    public void deveRetornarTarefaIncluida(){
+        TarefaDTO tarefaDTO = new TarefaDTO("Criar um site", "2000", LocalDate.now().plusDays(1L));
+        Tarefa tarefa = new Tarefa(tarefaDTO);
+
+        Mockito.when(tarefaRepository.save(Mockito.any(Tarefa.class))).thenReturn(tarefa);
+
+        Tarefa tarefaResponse = tarefaService.incluirTarefa(tarefaDTO);
+
+        assertEquals(tarefa, tarefaResponse);
     }
 
 }
